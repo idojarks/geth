f26b7198fff4e1f9459f9236c17ef270778691a4
idojarks <idojarks@gmail.com>
2022-06-12 11:45:45 +0900

    remove unused imports

--- app/src/main/java/com/example/geth/ui/screen/Main.kt
+++ app/src/main/java/com/example/geth/ui/screen/Main.kt
@@ -1,23 +1,10 @@
 package com.example.geth.ui.screen
 
-import androidx.annotation.StringRes
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.geth.R
 import com.example.geth.ui.AccountScreen
-import com.example.geth.ui.InfoScreen
 import com.example.geth.ui.screen.home.HomeScreen
 import com.example.geth.ui.screen.settings.SettingsScreen
 
@@ -38,6 +25,5 @@ fun MainView() {
         composable(route = Screen.Settings.route) {
             SettingsScreen(navController = navController)
         }
-
     }
 }
