07be8e990ccaeca1223ffc565bf8f7f56143a523
idojarks <idojarks@gmail.com>
2022-06-17 04:54:41 +0900

    add AccountRepository.kt

--- app/src/main/java/com/example/geth/EtherViewModel.kt
+++ app/src/main/java/com/example/geth/EtherViewModel.kt
@@ -2,16 +2,20 @@ package com.example.geth
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.geth.data.AccountRepository
 import com.example.geth.http.Url
 
 interface EtherViewModelInterface {
     var ether: Ether?
     val buildModel: MutableLiveData<String>
     val web3ClientVersion: MutableLiveData<String>
-    val accounts: MutableLiveData<MutableList<EtherAccount>>
+    val accounts: MutableLiveData<List<EtherAccount>>
 
     fun init(): Boolean
+
+    fun loadAccounts()
     fun addAccount(block: () -> EtherAccount)
+    fun deleteAccount(account: EtherAccount)
 }
 
 class EtherViewModel : ViewModel(), EtherViewModelInterface {
@@ -20,13 +24,20 @@ class EtherViewModel : ViewModel(), EtherViewModelInterface {
             override var ether: Ether? = null
             override val buildModel: MutableLiveData<String> = MutableLiveData("test1")
             override val web3ClientVersion: MutableLiveData<String> = MutableLiveData("test2")
-            override val accounts: MutableLiveData<MutableList<EtherAccount>> = MutableLiveData(mutableListOf(EtherAccount("john", "0x00", "0x00")))
+            override val accounts: MutableLiveData<List<EtherAccount>> = MutableLiveData(listOf(EtherAccount("john", "0x00", "0x00")))
 
             override fun init(): Boolean {
                 return true
             }
 
+            override fun loadAccounts() {
+            }
+
             override fun addAccount(block: () -> EtherAccount) {
+            }
+
+            override fun deleteAccount(account: EtherAccount) {
+
             }
         }
     }
@@ -34,17 +45,28 @@ class EtherViewModel : ViewModel(), EtherViewModelInterface {
     override var ether: Ether? = null
     override val buildModel = MutableLiveData("")
     override val web3ClientVersion = MutableLiveData("")
-    override val accounts = MutableLiveData<MutableList<EtherAccount>>()
-
-    override fun addAccount(block: () -> EtherAccount) {
-        accounts.value?.add(block())
-    }
+    override val accounts = MutableLiveData<List<EtherAccount>>()
+    private val accountRepository = AccountRepository(
+        MyApplication.getContext(),
+        "accountsTable",
+    )
 
     override fun init(): Boolean {
         ether = Ether()
         ether?.init(Url().infuraRopsten, this)
+
         return true
     }
 
+    override fun loadAccounts() {
+        accounts.value = accountRepository.getAccounts()
+    }
 
+    override fun addAccount(block: () -> EtherAccount) {
+        accounts.value = accountRepository.addAccount(block())
+    }
+
+    override fun deleteAccount(account: EtherAccount) {
+        accounts.value = accountRepository.deleteAccount(account)
+    }
 }
---
+++ app/src/main/java/com/example/geth/data/AccountRepository.kt
@@ -0,0 +1,134 @@
+package com.example.geth.data
+
+import android.content.Context
+import android.widget.Toast
+import com.example.geth.EtherAccount
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.File
+
+class AccountRepository(
+    private val context: Context,
+    private val filename: String,
+) {
+    private val accounts = mutableListOf<EtherAccount>()
+
+    fun getAccounts(): List<EtherAccount> {
+        loadIfAccountsEmpty()
+
+        return accounts
+    }
+
+    fun addAccount(account: EtherAccount): List<EtherAccount> {
+        account.validateBeforeUse()
+        loadIfAccountsEmpty()
+
+        if (accounts.contains(account)) {
+            Toast.makeText(context, "account exists", Toast.LENGTH_SHORT)
+                .show()
+
+            return accounts
+        }
+
+        accounts.add(account)
+        updateAccountsToFile()
+
+        return accounts
+    }
+
+    fun deleteAccount(account: EtherAccount): List<EtherAccount> {
+        account.validateBeforeUse()
+        accounts.remove(account)
+        updateAccountsToFile()
+
+        return accounts
+    }
+
+    private fun loadIfAccountsEmpty() {
+        if (accounts.isNotEmpty()) {
+            return
+        }
+
+        val file = File(context.filesDir, filename)
+
+        if (!file.exists()) {
+            return
+        }
+
+        val sb = StringBuilder()
+
+        context.openFileInput(filename)
+            .bufferedReader()
+            .useLines { lines ->
+                lines.forEach {
+                    sb.append(it)
+                }
+            }
+
+        val json = JSONObject(sb.toString())
+        val array = json.getJSONArray("accounts")
+
+        if (array.length() == 0) {
+            return
+        }
+
+        for (i in 0..array.length()) {
+            val account = array.getJSONObject(i)
+            EtherAccount(
+                name = account.get("name")
+                    .toString(),
+                address = account.get("address")
+                    .toString(),
+                privateKey = account.get("pk")
+                    .toString(),
+            ).also {
+                accounts.add(it)
+            }
+        }
+    }
+
+    private fun updateAccountsToFile() {
+        File(context.filesDir, filename).also {
+            if (it.exists()) {
+                it.delete()
+            }
+        }
+
+        context.openFileOutput(filename, Context.MODE_PRIVATE)
+            .use { outputStream ->
+                val array = JSONArray()
+
+                accounts.forEachIndexed { index, etherAccount ->
+                    JSONObject().put("name", etherAccount.name)
+                        .put("address", etherAccount.address)
+                        .put("pk", etherAccount.privateKey)
+                        .also {
+                            array.put(index, it)
+                        }
+                }
+
+                val json = JSONObject().put("accounts", array)
+
+                outputStream.write(json.toString()
+                    .toByteArray())
+            }
+    }
+}
+
+fun EtherAccount.validateBeforeUse() {
+    if (name.isEmpty()) {
+        throw IllegalArgumentException("invalid account: empty Name")
+    } else if (address.isEmpty()) {
+        throw IllegalArgumentException("invalid account: empty Address")
+    } else if (privateKey.isEmpty()) {
+        throw IllegalArgumentException("invalid account: empty PrivateKey")
+    }
+}
+
+infix fun AccountRepository.add(account: EtherAccount): List<EtherAccount> {
+    return addAccount(account)
+}
+
+infix fun AccountRepository.delete(account: EtherAccount): List<EtherAccount> {
+    return deleteAccount(account)
+}
