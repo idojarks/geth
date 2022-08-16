b2927cc2ccdbcb9e860ed316106299402073a50c
idojarks <idojarks@gmail.com>
2022-08-16 22:10:45 +0900

    change artwork card to outlined card link to artwork detail

--- app/src/main/java/com/example/geth/data/ArtworkToken.kt
+++ app/src/main/java/com/example/geth/data/ArtworkToken.kt
@@ -4,7 +4,6 @@ import com.example.geth.Contracts_Dragon721_sol_Dragon721
 
 data class ArtworkToken(
     val index: Int = -1,
-    var imageUrl: String = "",
     val context: Contracts_Dragon721_sol_Dragon721.Artwork = Contracts_Dragon721_sol_Dragon721.Artwork("", "", ""),
 ) {
     open class LoadingTokenState {
--- app/src/main/java/com/example/geth/ui/screen/home/route/dragon721/Dragon721ArtworkDetailScreen.kt
+++ app/src/main/java/com/example/geth/ui/screen/home/route/dragon721/Dragon721ArtworkDetailScreen.kt
@@ -1,5 +1,7 @@
 package com.example.geth.ui.screen.home.route.dragon721
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
@@ -8,5 +10,10 @@ fun Dragon721ArtworkDetailScreen(
     title: String,
     artist: String,
 ) {
+    Column {
+        Text(text = tokenId.toString())
+        Text(text = title)
+        Text(text = artist)
+    }
 
 }
